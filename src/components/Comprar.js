4f533dc6637bbea21e14221ea6a259d6293137e2
esionascimento <esio.392@protonmail.com>
2021-11-10 18:02:43 -0400

    concluded tag a para escolher moto ou carro

--- src/components/Comprar.js
+++ src/components/Comprar.js
@@ -6,20 +6,39 @@ import './Comprar.css';
 export const Comprar = () => {
   return (
     <div className="box">
-      <div className="boxLogo">
+      <header className="boxLogo">
         <img className="logo" src={ logo } alt="logo da empresa web motors"/>
-      </div>
-      <div className="boxEscolher">
-        <a href="http">
-          <img className="motoCar" src="https://cdn.webmotors.com.br/webmotors/Content/assets/img/car_icon_gray.svg" alt="logo da empresa web motors"/>
-          <div>
-            <p>COMPRAR</p>
-            <p>CARROS</p>
-          </div>
-        </a>
-        <a href="https://">
-          <img className="motoCar" src="https://cdn.webmotors.com.br/webmotors/Content/assets/img/bike_icon_gray.svg" alt="logo da empresa web motors"/>
-        </a>
+      </header>
+      <div>
+        <div className="boxEscolher">
+          <a className="ativar"href="http">
+            <div className="ad carro">
+              <img className="motoCar" src="https://cdn.webmotors.com.br/webmotors/Content/assets/img/car_icon_gray.svg" alt="logo da empresa web motors"/>
+              <div>
+                <p className="textComprar">COMPRAR</p>
+                <bold>CARROS</bold>
+              </div>
+            </div>
+          </a>
+          <a href="https://">
+            <div className="ad moto">
+              <img className="motoCar" src="https://cdn.webmotors.com.br/webmotors/Content/assets/img/bike_icon_gray.svg" alt="logo da empresa web motors"/>
+              <div>
+                <p className="textComprar">COMPRAR</p>
+                <p>MOTOS</p>
+              </div>
+            </div>
+          </a>
+        </div>
+        <a className="ativar"href="http">
+            <div className="ad carro">
+              <img className="motoCar" src="https://cdn.webmotors.com.br/webmotors/Content/assets/img/car_icon_gray.svg" alt="logo da empresa web motors"/>
+              <div>
+                <p className="textComprar">COMPRAR</p>
+                <bold>CARROS</bold>
+              </div>
+            </div>
+          </a>
       </div>
       <div></div>
       <div></div>
