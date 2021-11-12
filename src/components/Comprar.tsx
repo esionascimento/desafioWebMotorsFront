02bfb5b8e50c1c5d38a106e484e4e2b6dc720d6f
esionascimento <esio.392@protonmail.com>
2021-11-12 16:21:12 -0400

    refatoraçao da tag a para distinguir qual tag esta ativa

--- src/components/Comprar.tsx
+++ src/components/Comprar.tsx
@@ -1,41 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import logo from '../img/logo.png';
 import { BoxConfigurar } from './BoxConfigurar';
-/* import './Comprar.ts'; */
 import * as S from './ts/Comprar';
 
 export const Comprar = () => {
+  const [active, setActive] = useState(false);
+
+  function ativar() {
+    setActive(true);
+  }
+
   return (
     <S.Box>
       <header>
-        <img className="logo" src={ logo } alt="logo da empresa web motors"/>
+        <S.Logo src={ logo } alt="logo da empresa web motors"/>
       </header>
-      <div className="boxComponent">
-        <div className="boxEscolher">
-          <a className="ativar"href="http">
-            <div className="ad carro">
-              <img className="motoCar" src="https://cdn.webmotors.com.br/webmotors/Content/assets/img/car_icon_gray.svg" alt="logo da empresa web motors"/>
+      <S.BoxComponent>
+        <S.BoxEscolher className="boxEscolher">
+          <S.TagA if={!active} href="http" onClick={ativar}>
+            <S.AdFlex className="ad carro">
+              <S.MotoCar className="MotoCar" src="https://cdn.webmotors.com.br/webmotors/Content/assets/img/car_icon_gray.svg" alt="logo da empresa web motors"/>
               <div>
                 <p className="textComprar">COMPRAR</p>
                 <p>CARROS</p>
               </div>
-            </div>
-          </a>
-          <a href="https://">
-            <div className="ad moto">
-              <img className="motoCar" src="https://cdn.webmotors.com.br/webmotors/Content/assets/img/bike_icon_gray.svg" alt="logo da empresa web motors"/>
+            </S.AdFlex>
+          </S.TagA>
+          <S.TagA if={active} onClick={ativar} href="https://">
+            <S.AdFlex className="ad moto">
+              <S.MotoCar className="MotoCar" src="https://cdn.webmotors.com.br/webmotors/Content/assets/img/bike_icon_gray.svg" alt="logo da empresa web motors"/>
               <div>
                 <p className="textComprar">COMPRAR</p>
                 <p>MOTOS</p>
               </div>
-            </div>
-          </a>
-        </div>
+            </S.AdFlex>
+          </S.TagA>
+        </S.BoxEscolher>
         <a className="vender"href="http">
           <p className="venderCarro">Vender meu carro</p>
         </a>
-      </div>
+      </S.BoxComponent>
       <BoxConfigurar />
     </S.Box>
   );
